3e4b880cb73507c025348e1fb153fc0e0bb950cb
ivanstnsk (via Travis CI) <ivan.stnsk@gmail.com>
2020-07-27 09:08:13 +0000

    Deploy ivanstnsk/monoplan-poc to github.com/ivanstnsk/monoplan-poc.git:gh-pages

---
+++ precache-manifest.316dd50925d0aa2ebc7445b2b26ec97c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4883a179ea3ebe42483c121bde29ee7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32df9e8931e3dcbf4e8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41e729431bf190b4b92d",
+    "url": "/static/js/2.10ac838c.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/static/js/2.10ac838c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32df9e8931e3dcbf4e8",
+    "url": "/static/js/main.44b857f2.chunk.js"
+  },
+  {
+    "revision": "9058575267335f01c8ac",
+    "url": "/static/js/runtime-main.97efa89a.js"
+  }
+]);
