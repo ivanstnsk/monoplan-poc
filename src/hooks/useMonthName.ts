db7ae6892c17b13eee300d2455988acb4abb47cd
ivanstnsk <ivan.stnsk@gmail.com>
2020-07-29 21:15:34 +0300

    Add useMonth hook

---
+++ src/hooks/useMonthName.ts
@@ -0,0 +1,23 @@
+type Hook = string;
+
+const MONTHS = {
+  '0': 'Січень',
+  '1': 'Лютий',
+  '2': 'Березень',
+  '3': 'Квітень',
+  '4': 'Травень',
+  '5': 'Червень',
+  '6': 'Липень',
+  '7': 'Серпень',
+  '8': 'Вересень',
+  '9': 'Жовтень',
+  '10': 'Листопад',
+  '11': 'Грудень',
+};
+
+
+export const useMonthName = (month: number): Hook => {
+  const monthName = MONTHS[month] || 'Invalid month';
+
+  return monthName;
+}
