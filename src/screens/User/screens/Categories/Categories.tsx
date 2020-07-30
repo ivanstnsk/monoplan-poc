47503af9d30701aecd3fdebb92ab83b32d863682
ivanstnsk <ivan.stnsk@gmail.com>
2020-07-30 03:53:29 +0300

    Update Categories screen

--- src/screens/User/screens/Categories/Categories.tsx
+++ src/screens/User/screens/Categories/Categories.tsx
@@ -1,56 +1,114 @@
 import * as React from 'react';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import MaterialTable, { Column } from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/EditOutlined';
+import Search from '@material-ui/icons/Search';
 
-import { Category } from '../../../../store/categories/categories.types';
+import { Category, CategoryType } from '../../../../store/categories/categories.types';
 
 import { useStore } from './useStore';
 import { useStyles } from './styles';
 
-const renderTable = (categories: Array<Category>, title: string): JSX.Element => (
-  <Grid item xs={12} md={6}>
-    <Typography component="h2" variant="h5">
-      {title}
-    </Typography>
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Category</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {categories.map((category) => (
-            <TableRow key={category.id}>
-              <TableCell component="th" scope="row">
-                {category.name}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </Grid>
-);
+const COLUMNS = [
+  { title: 'category', field: 'name' },
+];
+
+const tableIcons = {
+  Add: React.forwardRef((props: any, ref) => <AddBox {...props} ref={ref} />),
+  Check: React.forwardRef((props: any, ref) => <Check {...props} ref={ref} />),
+  Clear: React.forwardRef((props: any, ref) => <Clear {...props} ref={ref} />),
+  Delete: React.forwardRef((props: any, ref) => <DeleteOutline {...props} ref={ref} />),
+  Edit: React.forwardRef((props: any, ref) => <Edit {...props} ref={ref} />),
+  ResetSearch: React.forwardRef((props: any, ref) => <Clear {...props} ref={ref} />),
+  Search: React.forwardRef((props: any, ref) => <Search {...props} ref={ref} />),
+  SortArrow: React.forwardRef((props: any, ref) => <ArrowDownward {...props} ref={ref} />),
+};
+
+const renderTable = (
+  categories: Array<Category>,
+  title: string,
+  onRowAdd: (newData: any) => Promise<void>,
+  onRowUpdate: (newData: any, oldData: any) => Promise<void>,
+  onRowDelete: (oldData: any) => Promise<void>,
+): JSX.Element => (
+    <Grid item xs={12} md={6}>
+      <MaterialTable
+        title={title}
+        columns={COLUMNS}
+        data={categories}
+        editable={{
+          onRowAdd,
+          onRowUpdate,
+          onRowDelete,
+        }}
+        icons={tableIcons as any}
+        options={{
+          paging: false,
+          search: false,
+        }}
+        localization={{
+          header: {
+            actions: '',
+          },
+          body: {
+            editRow: {
+              deleteText: 'Видалити цю категорію?',
+            },
+            deleteTooltip: 'Видалити'
+          }
+        }}
+      />
+    </Grid>
+  );
 
 export const Categories: React.FC = () => {
   const classes = useStyles();
   const Store = useStore();
 
+  const getRowAddHandler = React.useCallback((categoryType: CategoryType) => {
+    return async (data: any): Promise<void> => {
+      Store.onAddCategory(categoryType, data.name);
+      return Promise.resolve();
+    }
+  }, []);
+
+  const getRowUpdateHandler = React.useCallback((categoryType: CategoryType) => {
+    return async (data: any, oldData: any): Promise<void> => {
+      Store.onUpdateCategory(categoryType, data.id, data.name)
+      return Promise.resolve();
+    }
+  }, []);
+
+  const getRowDeleteHandler = React.useCallback((categoryType: CategoryType) => {
+    return async (data: any): Promise<void> => {
+      const { id } = data;
+      Store.onDeleteCategory(categoryType, id);
+      return Promise.resolve();
+    }
+  }, []);
+
   return (
     <div className={classes.paper}>
       <Grid container spacing={4} className={classes.tableWrapper}>
-        {renderTable(Store.expenses, 'Expenses')}
-        {renderTable(Store.income, 'Income')}
+        {renderTable(
+          Store.expenses,
+          'Expenses',
+          getRowAddHandler('expenses'),
+          getRowUpdateHandler('expenses'),
+          getRowDeleteHandler('expenses')
+        )}
+        {renderTable(
+          Store.income,
+          'Income',
+          getRowAddHandler('income'),
+          getRowUpdateHandler('income'),
+          getRowDeleteHandler('income')
+        )}
       </Grid>
     </div>
   );
