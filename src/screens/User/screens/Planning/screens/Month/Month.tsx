079319168d5be68947a77fe6266d1b6f0b0772b5
ivanstnsk <ivan.stnsk@gmail.com>
2020-07-30 03:53:20 +0300

    Update Month screen

--- src/screens/User/screens/Planning/screens/Month/Month.tsx
+++ src/screens/User/screens/Planning/screens/Month/Month.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
+import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { useParams } from 'react-router-dom';
 
+import MaterialTable, { Column } from 'material-table';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import Clear from '@material-ui/icons/Clear';
+import Edit from '@material-ui/icons/EditOutlined';
+
+import { Category, CategoryType } from '../../../../../../store/categories/categories.types';
 import { getMonthNameByIndex } from '../../../../../../utils';
 
-import { useStore } from './useStore';
+import { ProgressBar } from './components';
+import { useStore, CategoryPrognosis, Balance } from './useStore';
 import { useStyles } from './styles';
 
 type RouteParams = {
@@ -22,156 +30,149 @@ type RouteParams = {
   month?: string;
 }
 
+const CATEGORIES_COLUMNS = [
+  { title: 'category', field: 'name', editable: 'never' },
+  { title: 'prognosis', field: 'prognosis' },
+  { title: 'actual', field: 'actual' },
+  { title: 'difference', field: 'difference', editable: 'never' },
+];
+
+const tableIcons = {
+  Check: React.forwardRef((props: any, ref) => <Check {...props} ref={ref} />),
+  Clear: React.forwardRef((props: any, ref) => <Clear {...props} ref={ref} />),
+  Edit: React.forwardRef((props: any, ref) => <Edit {...props} ref={ref} />),
+  SortArrow: React.forwardRef((props: any, ref) => <ArrowDownward {...props} ref={ref} />),
+};
+
+const renderBalanceTable = (
+  title: string,
+  balance: Balance,
+  classes: any,
+  color: string,
+): JSX.Element => (
+    <Grid item xs={12} md={6}>
+      <Typography component="h2" variant="h5">
+        {title}
+      </Typography>
+      <Card className={classes.card}>
+        <div className={classes.cardDetails}>
+          <CardContent>
+            <Grid container>
+              <Grid xs={3} md={3}>
+                <Typography component="div" variant="body1">Prognosis</Typography>
+              </Grid>
+              <Grid xs={3} md={3}>
+                <Typography component="div" variant="body1">
+                  {balance.prognosis}
+                </Typography>
+              </Grid>
+              <Grid xs={6} md={6}>
+                <ProgressBar color={color} progress={0} />
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid xs={3} md={3}>
+                <Typography component="div" variant="body1">Actual</Typography>
+              </Grid>
+              <Grid xs={3} md={3}>
+                <Typography component="div" variant="body1">
+                  {balance.actual}
+                </Typography>
+              </Grid>
+              <Grid xs={6} md={6}>
+                <ProgressBar color={color} progress={59} />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </div>
+      </Card>
+    </Grid>
+  );
+
+const renderCategoriesTable2 = (
+  categories: Array<CategoryPrognosis>,
+  title: string,
+  onRowUpdate: (newData: any, oldData: any) => Promise<void>,
+): JSX.Element => (
+    <Grid item xs={12} md={6}>
+      <MaterialTable
+        title={title}
+        columns={CATEGORIES_COLUMNS as any}
+        data={categories}
+        editable={{
+          onRowUpdate,
+        }}
+        icons={tableIcons as any}
+        options={{
+          paging: false,
+          search: false,
+        }}
+        localization={{
+          header: {
+            actions: '',
+          },
+          body: {
+            editRow: {
+              deleteText: 'Видалити цю категорію?',
+            },
+            deleteTooltip: 'Видалити'
+          }
+        }}
+      />
+    </Grid>
+  );
+
 export const Month: React.FC = () => {
   const classes = useStyles();
   const params = useParams<RouteParams>();
   const Store = useStore(params.year, params.month);
 
-  console.log(Store.monthPlan)
+  const getRowUpdateHandler = React.useCallback((categoryType: CategoryType) => {
+    return async (data: any, oldData: any): Promise<void> => {
+      console.log(data);
+      // Store.onUpdateCategory(categoryType, data.id, data.name)
+      return Promise.resolve();
+    }
+  }, []);
+
   return (
     <div className={classes.paper}>
-      {!Store.monthPlan && (
-        <div>Invalid month plan data</div>
-      )}
-      {Store.monthPlan && (
-        <>
-          {params.month && params.year && (
-            <div className={classes.titleContainer}>
-              <Typography component="h1" variant="h4">
-                {`${getMonthNameByIndex(params.month)} ${params.year}`}
-              </Typography>
-            </div>
+      <>
+        {params.month && params.year && (
+          <div className={classes.titleContainer}>
+            <Typography component="h1" variant="h4">
+              {`${getMonthNameByIndex(params.month)} ${params.year}`}
+            </Typography>
+          </div>
+        )}
+        <Grid container spacing={4}>
+          {renderBalanceTable(
+            'Expenses',
+            Store.expenses.balance,
+            classes,
+            '#9E3D3D',
           )}
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={6}>
-              <Typography component="h2" variant="h5">
-                Expenses
-              </Typography>
-              <Card className={classes.card}>
-                <div className={classes.cardDetails}>
-                  <CardContent>
-                    <Grid container>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">Prognosis</Typography>
-                      </Grid>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">
-                          {Store.monthPlan.prognosis.income}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                    <Grid container>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">Actual</Typography>
-                      </Grid>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">
-                          {Store.monthPlan.actual.income}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </CardContent>
-                </div>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Typography component="h2" variant="h5">
-                Income
-              </Typography>
-              <Card className={classes.card}>
-                <div className={classes.cardDetails}>
-                  <CardContent>
-                    <Grid container>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">Prognosis</Typography>
-                      </Grid>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">
-                          {Store.monthPlan.prognosis.expenses}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                    <Grid container>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">Actual</Typography>
-                      </Grid>
-                      <Grid xs={3} md={3}>
-                        <Typography component="div" variant="body1">
-                          {Store.monthPlan.actual.expenses}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </CardContent>
-                </div>
-              </Card>
-            </Grid>
-          </Grid>
+          {renderBalanceTable(
+            'Income',
+            Store.income.balance,
+            classes,
+            '#539E3D',
+          )}
+        </Grid>
 
-          <Grid container spacing={4} className={classes.tableWrapper}>
-            <Grid item xs={12} md={6}>
-              <Typography component="h2" variant="h5">
-                Expenses
-              </Typography>
-              <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Category</TableCell>
-                      <TableCell align="right">Prognosis</TableCell>
-                      <TableCell align="right">Actual</TableCell>
-                      <TableCell align="right">Difference</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  {/* <TableBody>
-                    {rows.map((row) => (
-                      <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody> */}
-                </Table>
-              </TableContainer>
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <Typography component="h2" variant="h5">
-                Income
-              </Typography>
-              <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Category</TableCell>
-                      <TableCell align="right">Prognosis</TableCell>
-                      <TableCell align="right">Actual</TableCell>
-                      <TableCell align="right">Difference</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  {/* <TableBody>
-                    {rows.map((row) => (
-                      <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody> */}
-                </Table>
-              </TableContainer>
-            </Grid>
-          </Grid>
-        </>
-      )}
+        <Grid container spacing={4} className={classes.tableWrapper}>
+          {renderCategoriesTable2(
+            Store.expenses.categories,
+            'Expenses',
+            getRowUpdateHandler('expenses'),
+          )}
+          {renderCategoriesTable2(
+            Store.income.categories,
+            'Income',
+            getRowUpdateHandler('income'),
+          )}
+        </Grid>
+      </>
     </div>
   );
 }
--- src/screens/User/screens/Planning/screens/Month/useStore.ts
+++ src/screens/User/screens/Planning/screens/Month/useStore.ts
@@ -2,25 +2,106 @@ import { useSelector } from 'react-redux';
 
 import { RootState } from '../../../../../../store/store.types';
 import { PlanningState, Plan } from '../../../../../../store/planning/planning.types';
+import { CategoriesState } from '../../../../../../store/categories/categories.types';
+
+export type CategoryPrognosis = {
+  id: string;
+  name: string;
+  prognosis: number;
+  actual: number;
+  difference: number;
+}
+
+export type Balance = {
+  prognosis: number;
+  actual: number;
+}
 
 type Hook = {
-  monthPlan: Plan | undefined;
+  income: {
+    balance: Balance;
+    categories: Array<CategoryPrognosis>;
+  }
+  expenses: {
+    balance: Balance;
+    categories: Array<CategoryPrognosis>;
+  }
 }
 
 export const useStore = (year: string | undefined, month: string | undefined): Hook => {
   const yearKey = year ? parseInt(year) : NaN;
   const monthKey = month || 'invalid';
-  console.log(yearKey, monthKey)
-  const planning = useSelector<RootState, PlanningState>(state => state.planning);
 
-  let monthPlan;
+  const planning = useSelector<RootState, PlanningState>(state => state.planning);
+  // const categories = useSelector<RootState, CategoriesState>(state => state.categories);
+  // const { income, expenses } = categories;
+
+  let monthPlan: Plan = {
+    income: [],
+    expenses: [],
+  };
 
   const plan = planning.plans.find((plan) => plan.year === yearKey);
   if (plan) {
     monthPlan = plan.months[monthKey];
   }
 
+  const { income, expenses } = monthPlan;
+
+  // const incomePrognosis = income.map((category) => {
+  //   const prognosis = 100;
+  //   const actual = 50;
+  //   const difference = prognosis - actual;
+
+  //   return {
+  //     id: category.id,
+  //     name: category.name,
+  //     prognosis,
+  //     actual,
+  //     difference,
+  //   }
+  // });
+
+  // const expensesPrognosis = income.map((category) => {
+  //   const prognosis = 100;
+  //   const actual = 50;
+  //   const difference = prognosis - actual;
+
+  //   return {
+  //     id: category.id,
+  //     name: category.name,
+  //     prognosis,
+  //     actual,
+  //     difference,
+  //   }
+  // });
+
+  const incomeBalance = {
+    prognosis: income.reduce((acc, category) => {
+      return acc += category.prognosis;
+    }, 0),
+    actual: income.reduce((acc, category) => {
+      return acc += category.actual;
+    }, 0),
+  }
+
+  const expensesBalance = {
+    prognosis: expenses.reduce((acc, category) => {
+      return acc += category.prognosis;
+    }, 0),
+    actual: expenses.reduce((acc, category) => {
+      return acc += category.actual;
+    }, 0),
+  }
+
   return {
-    monthPlan,
+    income: {
+      balance: incomeBalance,
+      categories: income
+    },
+    expenses: {
+      balance: expensesBalance,
+      categories: expenses
+    }
   }
 }
